09f77b416754e664faeb44a58ee25e466687ebea
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2023-07-26 17:08:04 +0530

    Time: 2 ms (36.01%), Space: 6.1 MB (57.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    int rec(int n,vector<int>& dp){
-        if(n==2) return 2;
-        if(n==1) return 1;
-        if(dp[n]!=-1) return dp[n];
-        int a = rec(n-1,dp);
-        int b = rec(n-2,dp);
-        return dp[n] = a+b;
-    }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return rec(n,dp);
+        vector<int> dp(n+1,0);
+        dp[1] = 1;
+        if(n>1)
+        dp[2] = 2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
