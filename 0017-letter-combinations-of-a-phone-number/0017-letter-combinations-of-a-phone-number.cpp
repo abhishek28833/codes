b25e1a181ba5fff36aca45f252d9a03f546554c6
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2023-08-03 22:25:32 +0530

    Time: 0 ms (100%), Space: 6.5 MB (31.4%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
+    vector<string> mp = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    
     vector<string> ans;
-    void dfs(unordered_map<char,vector<char>>& mp,int i,string digits,int n,string str){
+    void dfs(int i,string digits,int n,string str){
         if(str.size()==n){
             ans.push_back(str);
             return;
@@ -9,51 +11,18 @@ public:
         if(i==n){
             return;
         }
-        int k = mp[digits[i]].size();
+        int k = mp[digits[i] - '0'].size();
         for(int j=0;j<k;j++){
-            str += mp[digits[i]][j];
-            dfs(mp,i+1,digits,n,str);
+            str += mp[digits[i] - '0'][j];
+            dfs(i+1,digits,n,str);
             str.pop_back();
         }
-
     }
 
     vector<string> letterCombinations(string digits) {
-        unordered_map<char,vector<char>> mp;
-
-        mp['2'].push_back('a');
-        mp['2'].push_back('b');
-        mp['2'].push_back('c');
-        mp['3'].push_back('d');
-        mp['3'].push_back('e');
-        mp['3'].push_back('f');
-        mp['4'].push_back('g');
-        mp['4'].push_back('h');
-        mp['4'].push_back('i');
-        mp['5'].push_back('j');
-        mp['5'].push_back('k');
-        mp['5'].push_back('l');
-        mp['6'].push_back('m');
-        mp['6'].push_back('n');
-        mp['6'].push_back('o');
-        mp['7'].push_back('p');
-        mp['7'].push_back('q');
-        mp['7'].push_back('r');
-        mp['7'].push_back('s');
-        mp['8'].push_back('t');
-        mp['8'].push_back('u');
-        mp['8'].push_back('v');
-        mp['9'].push_back('w');
-        mp['9'].push_back('x');
-        mp['9'].push_back('y');
-        mp['9'].push_back('z');
-
         int n = digits.size();
         if(n==0) return ans;
-        dfs(mp,0,digits,n,"");
-        return ans;
-        
-
-
-    }
+        dfs(0,digits,n,"");
+        return ans;
+    }
 };
