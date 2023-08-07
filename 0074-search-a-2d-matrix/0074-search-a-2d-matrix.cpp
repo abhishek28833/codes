eb777e285967a7c684deb914f2f36e7f4d7cf92d
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2023-08-07 09:52:33 +0530

    Time: 3 ms (82.89%), Space: 9.4 MB (90.26%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
+    int binary_search(vector<vector<int>>& matrix,int target,int i,int low,int high){
+        if(low>high) return 0;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(matrix[i][mid]==target) return 1;
+            if(matrix[i][mid]<target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return 0;
+    }
+
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-        int p = 0;
-        if(m==1 && n==1 && matrix[0][0]== target)
-        return 1;
-        else if(m==1 && n==1)
-        return 0;
-        cout<<p;
-        for(int i = 1;i<n;i++){
-            if(target > matrix[i-1][m-1] && target <= matrix[i][m-1]){
-                p=i;
-                break;
+        int low = 0,high = n-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(matrix[mid][0]<=target && matrix[mid][m-1]>=target && binary_search(matrix,target,mid,0,m-1)){
+                return true;
             }
-            
-        }
-        int start = 0;
-        int end = m-1;
-        
-        while(start<=end){
-            int mid = (start+end)/2;
-            if(matrix[p][mid] == target)
-            return 1;
-            else if(target < matrix[p][mid]){
-                end = mid-1;
+            if(target>matrix[mid][m-1]){
+                low = mid+1;
             }
-            else
-            start = mid+1;
+            else{
+                high = mid-1;
+            }
         }
-        return 0;
+        return false;
     }
 };
