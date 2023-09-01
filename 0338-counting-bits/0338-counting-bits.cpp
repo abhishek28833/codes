ddf679152804e6f6f9d6543d623d939d4ed1685b
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2023-09-01 10:00:24 +0530

    Time: 9 ms (25.59%), Space: 9.2 MB (5.98%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int rec(int n){
+    int rec(int n,vector<int>& dp){
         if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
         int k = log2(n);
-        int a =0;
-        a = 1 + rec(n-(pow(2,k)));
-        return a;
+        int a = 1 + rec(n-(pow(2,k)),dp);
+        return dp[n] = a;
     }
 
     vector<int> countBits(int n) {
         vector<int> ans;
+        vector<int> dp(n+1,-1);
         for(int i=0;i<=n;i++){
-            ans.push_back(rec(i));
+            ans.push_back(rec(i,dp));
         }
         return ans;
     }
