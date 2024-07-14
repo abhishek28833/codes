b1caf76deff055a09c5c1562940995b25296f1ca
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2024-07-14 12:43:58 +0530

    Time: 506 ms (44.44%), Space: 234.8 MB (11.11%) - LeetHub

---
+++ 3500-minimum-cost-for-cutting-cake-ii/3500-minimum-cost-for-cutting-cake-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long minimumCost(int m, int n, vector<int>& hori, vector<int>& verti) {
+        sort(hori.rbegin(),hori.rend());
+        sort(verti.rbegin(),verti.rend());
+        int i=0,j=0,x=0,y=0;
+        long long ans = 0;
+        while(i<m-1 && j<n-1){
+            if(hori[i]>verti[j]){
+                ans += (hori[i]*(y+1));
+                i++;
+                x++;
+            }
+            else{
+                ans+=(verti[j]*(x+1));
+                j++;
+                y++;
+            }
+        }
+        while(i<m-1){
+            ans += (hori[i]*(y+1));
+            i++;
+        }
+        while(j<n-1){
+            ans+=(verti[j]*(x+1));
+            j++;
+        }
+        return ans;
+    }
+};
