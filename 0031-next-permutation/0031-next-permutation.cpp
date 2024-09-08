8e1cc54557726f9cb0ee8a920ecbe9c1202a2d46
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2024-09-08 20:13:45 +0530

    Time: 0 ms (100%), Space: 14.9 MB (52.38%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int fn(vector<int>& nums,int i,int n,int val){
-        sort(nums.begin()+i,nums.end());
-        for(int j=i;j<n;j++){
-            if(nums[j]>val) return j;
-        }
-        return 0;
-    }
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
+        int save = -1,prev = -1;
         for(int i=n-1;i>0;i--){
             if(nums[i]>nums[i-1]){
-                int a = fn(nums,i-1,n,nums[i-1]);
-                swap(nums[i-1],nums[a]);
-                sort(nums.begin()+i,nums.end());
-                return;
+                int val = nums[i-1];
+                int maxi= INT_MAX,ind = i-1;
+                for(int j=i;j<n;j++){
+                    if(nums[j]>val && nums[j]<maxi){
+                        maxi = min(nums[j],maxi);
+                        ind = j;
+                    }
+                }
+                if(maxi != INT_MAX){
+                    nums[i-1]=nums[ind];
+                    nums[ind]=val;
+                    sort(nums.begin()+i,nums.end());
+                    return;
+                }
             }
         }
         sort(nums.begin(),nums.end());
-
     }
 };
