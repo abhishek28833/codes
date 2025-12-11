fa3d72d7155783ce5901adb32cac906cb0cbd31d
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2025-12-11 23:16:58 +0530

    Time: 113 ms (8.55%), Space: 47.3 MB (7.84%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -2,18 +2,26 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                Map<Long,Integer> mp = new HashMap<>();
-                for(int k = j+1;k<nums.length;k++){
-                    long need = (long)target - nums[i] - nums[j] - nums[k];
-                    
-                    if(mp.containsKey(need)) {
-                        List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], nums[mp.get(need)]);
-                        if(!ans.contains(temp)) ans.add(temp);
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                
+                int left = j+1;
+                int right = n-1;
+                
+                while(left < right){
+                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+                    if(sum == target){
+                        List<Integer> temp = new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+                        if(!ans.contains(temp))
+                            ans.add(temp);
+                        left+=1;
+                        right-=1;    
+                    }else if(sum > target){
+                        right--;
+                    }else{
+                        left++;
                     }
-                    
-                    mp.put((long)nums[k], k);
                 }
             }
         }
