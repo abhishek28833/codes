e2ee6505eaed29069df49f98de54a3962703f997
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2026-01-08 00:47:29 +0530

    Time: 64 ms (64.77%), Space: 59.3 MB (93.04%) - LeetHub

---
+++ 3799-word-squares-ii/3799-word-squares-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> wordSquares(vector<string>& words) {
+        int n = words.size();
+        vector<vector<string>> ans;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(j==i) continue;
+                for(int k=0;k<n; k++){
+                    if(k==i || k==j) continue;
+                    for(int l=0; l<n; l++){
+                        if(l==i || l==j || l==k) continue;
+                        // cout<<i<<" "<<j<<" "<<k<<" "<<" "<<l<<"\n";
+                        if(words[i][0] == words[j][0] && words[i][3] == words[k][0] && words[j][3] == words[l][0] && words[k][3] == words[l][3]){
+                            ans.push_back({words[i],words[j],words[k],words[l]});
+                        }
+                    }
+                }
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
