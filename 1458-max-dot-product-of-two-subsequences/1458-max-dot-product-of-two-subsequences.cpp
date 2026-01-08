b5253c8e4990fe91950a3e1d0a17f176c74dd1e2
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2026-01-08 23:34:58 +0530

    Time: 15 ms (76%), Space: 17.9 MB (32.4%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -2,26 +2,28 @@
 class Solution {
 public:
     // int ans = INT_MIN;
-    int rec(vector<int>& nums1, vector<int>& nums2, int i, int j,int n,int m,map<pair<int,int>,int>& mp){
+    int rec(vector<int>& nums1, vector<int>& nums2, int i, int j,int n,int m,vector<vector<int>>& dp){
         if(i >= n || j >= m){
             return 0;
         } 
         
-        if(mp.count({i,j})) return mp[{i,j}];
+        // if(mp.count({i,j})) return mp[{i,j}];
+        if(dp[i][j] != INT_MIN) return dp[i][j];
 
         int maxi = INT_MIN;
-        maxi = max(maxi, nums1[i]*nums2[j] + rec(nums1,nums2,i+1,j+1,n,m,mp));
-        maxi = max(maxi,rec(nums1,nums2,i,j+1,n,m,mp));
-        maxi = max(maxi,rec(nums1,nums2,i+1,j,n,m,mp));
-        return mp[{i,j}] = maxi;
+        maxi = max(maxi, nums1[i]*nums2[j] + rec(nums1,nums2,i+1,j+1,n,m,dp));
+        maxi = max(maxi,rec(nums1,nums2,i,j+1,n,m,dp));
+        maxi = max(maxi,rec(nums1,nums2,i+1,j,n,m,dp));
+        return dp[i][j] = maxi;
     }
 
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
         
-        map<pair<int,int>,int> mp;
-        int ans = rec(nums1,nums2,0,0,n,m,mp);
+        // map<pair<int,int>,int> mp;
+        vector<vector<int>> dp(n+1,vector<int> (m+1,INT_MIN));
+        int ans = rec(nums1,nums2,0,0,n,m,dp);
         if(ans == 0){
             int maxi = INT_MIN;
             for(int i=0;i<n;i++){
