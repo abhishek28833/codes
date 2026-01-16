671574c8f0e3c743763a5aa2858f0026dde09003
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2026-01-16 22:15:45 +0530

    Time: 1005 ms (70.87%), Space: 250.4 MB (92.23%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -3,42 +3,43 @@ public:
     long long mod = 1e9+7;
 
     int maximizeSquareArea(int m, int n, vector<int>& hBars, vector<int>& vBars) {
-        
+        vector<int> arr,brr;
         hBars.push_back(m);
         vBars.push_back(n);
         sort(hBars.begin(),hBars.end());
         sort(vBars.begin(),vBars.end());
-
-        unordered_set<int> arr,brr;
         int c = hBars.size();
         int d = vBars.size();
         for(int i=0;i<c;i++){
             for(int j=0;j<i;j++){
-                arr.insert(hBars[i]-hBars[j]);
+                arr.push_back(hBars[i]-hBars[j]);
             }
-            arr.insert(hBars[i]-1);
+            arr.push_back(hBars[i]-1);
         }
-        arr.insert(m-1);
+        arr.push_back(m-1);
 
-        int ans = 0;
+
         for(int i=0;i<d;i++){
             for(int j=0;j<i;j++){
-                int hei = vBars[i]-vBars[j];
-                if(arr.count(hei)){
-                    ans = max(ans,hei);
-                }
+                brr.push_back(vBars[i]-vBars[j]);
             }
-            if(arr.count(vBars[i]-1)){
-                ans = max(ans,vBars[i]-1);
-            }
+            brr.push_back(vBars[i]-1);
         }
-        if(arr.count(n-1)){
-            ans = max(ans,n-1);
+        brr.push_back(n-1);
+
+
+        int ans = -1;
+        int a = arr.size(),b = brr.size();
+        sort(arr.begin(),arr.end());
+        sort(brr.begin(),brr.end());
+        int i=0,j=0;
+        while(i<a && j<b){
+            if(arr[i] == brr[j]){
+                ans = max(ans,arr[i]);
+                i++;j++;
+            }else if(arr[i] > brr[j]) j++;
+            else i++;
         }
-
-        return (ans == 0) ? -1: (1LL*ans*ans)%mod;
-
-        
-        return ans;
+        return (ans == -1) ? -1 : (1LL* ans * ans)%mod;
     }
 };
