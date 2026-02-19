24cd9fdcf53365a6600d74966df8075800984888
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2026-02-19 23:38:18 +0530

    Time: 3 ms (69.62%), Space: 12 MB (66.02%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    map<pair<string,int>,bool> dp;
-    bool rec(string& s,int n, string str,int i,unordered_set<string>& se){
+    unordered_map<int,bool> dp;
+    bool rec(string& s,int n,int i,unordered_set<string>& se){
         if(i == n){
-            if(se.count(str)) return true;
-            return false;
+            return true;
         }
 
         // cout<<str<<" ";
-        if(dp.count({str,i})) return dp[{str,i}];
 
-        bool flag = false;
-        if(se.count(str)){
-            string stemp(1,s[i]);
-            if(rec(s,n,stemp,i+1,se)) flag = true;
+        if(dp.count(i)) return dp[i];
+
+        string str = "";
+        for(int j=i;j<n;j++){
+            str.push_back(s[j]);
+            if(se.count(str)){
+                if(rec(s,n,j+1,se)) return dp[i] = true;
+            }
         }
 
-        if(rec(s,n,str + s[i],i+1,se)) flag = true;
-        return dp[{str,i}] = flag;
+        return dp[i] = false;
+        
     }
 
     bool wordBreak(string s, vector<string>& wordDict) {
@@ -25,7 +27,7 @@ public:
         for(auto a: wordDict){
             se.insert(a);
         }
-        return rec(s,s.size(),"",0,se);
+        return rec(s,s.size(),0,se);
 
     }
 };
