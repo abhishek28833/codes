dc4406b7d0570ac56b7e53ace6812bbde9b8216f
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2026-04-15 23:43:53 +0530

    Time: 1439 ms (9.98%), Space: 190.5 MB (30.51%) - LeetHub

--- 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.cpp
+++ 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.cpp
@@ -3,8 +3,9 @@ public:
     vector<int> prime;
 
     int checkPrime(int n){
-        if(n <= 1) return 0;
-        for(int i=2;i*i<=n;i++){
+        if(n == 2) return 1;
+        if(n <= 1 || n%2==0) return 0;
+        for(int i=3;i*i<=n;i+=2){
             if(n%i == 0) return 0;
         }
         return 1;
