a856e42a9b74b7c3732f67811deb9d0ee2e55ada
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2026-04-15 23:23:52 +0530

    Time: 2755 ms (5%), Space: 190.3 MB (30.52%) - LeetHub

---
+++ 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> prime;
+
+    int checkPrime(int n){
+        if(n <= 1) return 0;
+        for(int i=2;i*i<=n;i++){
+            if(n%i == 0) return 0;
+        }
+        return 1;
+    }
+
+    void primeset(){
+        for(int i=0;i<1e5+5;i++){
+            if(checkPrime(i)){
+                // cout<<i<<" ";
+                prime.push_back(i);
+            }
+            
+        }
+    }
+
+    int justGreaterPrime(int n){
+        int low = 0,high = prime.size(),ans = 1e5+3;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(prime[mid] >= n){
+                ans = min(ans,prime[mid]);
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+
+    }
+
+    int minOperations(vector<int>& nums) {
+        primeset();
+        int n = nums.size(),ans=0;
+        for(int i=0;i<n;i++){
+            if(i%2 == 0){
+                ans += justGreaterPrime(nums[i])-nums[i];
+            }else{
+                if(nums[i] == 2) ans += 2;
+                else if(checkPrime(nums[i])) ans++;
+            }
+            
+        }
+        return ans;
+    }
+};
