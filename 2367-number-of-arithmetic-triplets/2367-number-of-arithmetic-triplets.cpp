2edae1b29ffb8088fb85f8900de180f0ca8c628c
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2026-05-27 18:08:49 +0530

    Time: 0 ms (100%), Space: 11.6 MB (73.41%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,26 +1,17 @@
    class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-        int n = nums.size();
-        int cnt = 0;
+        int n = nums.size(),cnt = 0;
         for(int i=0;i<n-1;i++){
             int j = i+1,k=n-1;
             while(j<k){
                 if(nums[j] - nums[i] == diff){
                     if(nums[k] - nums[j] == diff){
-                        cnt++;
-                        j++;
-                        k--;
-                    }else if(nums[k] - nums[j] > diff){
-                        k--;
-                    }else{
-                        j++;
-                    }
-                }else if(nums[j] - nums[i] < diff){
-                    j++;
-                }else{
-                    break;
-                }
+                        cnt++;j++;k--;
+                    }else if(nums[k] - nums[j] > diff) k--;
+                    else j++;
+                }else if(nums[j] - nums[i] < diff) j++;
+                else break;
             }
         }
         return cnt;
