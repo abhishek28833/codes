afecfb25ab761de7361e12f155eb2ccb14d7282c
Abhishek Verma <103577678+abhishek28833@users.noreply.github.com>
2026-06-17 00:01:49 +0530

    Time: 0 ms (100%), Space: 10 MB (8.65%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,52 +1,33 @@
 class MyStack {
 public:
-    queue<int> q1,q2;
+    queue<int> q;
     MyStack() {
         
     }
-    int t;
+    
     void push(int x) {
-        if(!q2.empty()){
-            q2.push(x);
-            t = x;
-        }
-        else{
-            q1.push(x);
-            t = x;
+        int n = q.size();
+        q.push(x);
+        for(int i=0;i<n;i++){
+            int val = q.front();
+            q.pop();
+            q.push(val);
         }
     }
     
     int pop() {
-        if(q1.size()>=1 && q2.size()==0){
-            while(q1.size()>1){
-                t = q1.front();
-                q2.push(q1.front());
-                q1.pop();
-            }
-            int temp = q1.front();
-            q1.pop();
-            return temp;
-        }
-        else{
-            while(q2.size()>1){
-                t=q2.front();
-                q1.push(q2.front());
-                q2.pop();
-            }
-            int temp = q2.front();
-            q2.pop();
-            return temp;
-        }
-        return 0;
+        int val = q.front();
+        q.pop();
+        return val;
     }
     
     int top() {
-        return t;
+        return q.front();
     }
     
     bool empty() {
-        if(q1.empty() && q2.empty()) return true;
-        else return false;
+        if(q.size()) return false;
+        return true;
     }
 };
 
